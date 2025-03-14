896d0ce67f541eaa3d0059bab214d8bfac530af4
sahnoun122 <khadijasahnoun70@gmail.com>
2025-03-14 13:45:58 +0000

    ajouter les route

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,8 @@ use App\Http\Controllers\RayonController;
 use App\Http\Controllers\ProduitController;
 use App\Http\Controllers\CategorieController;
 use App\Http\Controllers\PromotionController;
+use App\Http\Controllers\DemandeController;
+
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -12,21 +14,33 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
+
+Route::apiResource('demande' ,DemandeController::class);
+
+
 Route::apiResource('rayon', RayonController::class);
+
 Route::apiResource('categorie', CategorieController::class);
 
 Route::apiResource('promotion', PromotionController::class);
 
-
 Route::apiResource('produit', ProduitController::class);
 
-Route::post('modifierStock' , [ProduitController::class , 'modifierStock']);
+// Route::post('modifierStock' , [ProduitController::class , 'modifierStock']);
 
 Route::get('recherche' , [ProduitController::class , 'recherche']); 
 
+// Route::get('Stockaible' , [ProduitController::class , 'StockFaible']);
 
-Route::get('StockFaible' , [ProduitController::class , 'StockFaible']);
+// Route::get('stock-faible' , [ProduitController::class , 'StockFaible']);  
 
-Route::get('StockFaible' , [ProduitController::class , 'StockFaible']);  
+Route::get('produit-demande' , [ProduitController::class , 'ProduitDemande']); 
 
-Route::get('ProduitDemande' , [ProduitController::class , 'ProduitDemande']); 
+
+Route::get('consulter-produit' , [DemandeController::class , 'consulterProduit']); 
+
+
+Route::get('recherche-produit' , [DemandeController::class , 'rechercheProduit']);
+
+Route::get('produits-populaire' , [DemandeController::class , 'rechercheProduit']); 
+
